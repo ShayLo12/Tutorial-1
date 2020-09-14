2250201b15e5b02d594fff839d2b5c6c2913e9b0
Shay Czachowski <70216050+ShayLo12@users.noreply.github.com>
2020-09-14 15:30:28 -0400

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+ 
+public class PlayerController : MonoBehaviour, RollABallControls.IPlayerActions {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public RollABallControls controls;
+    public Vector2 motion;
+
+    private Rigidbody rb;
+    private int count;
+
+    // Comment
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText ();
+        winText.text = "";
+    }
+
+    public void OnEnable () 
+    {
+        if (controls == null)
+        {
+            controls = new RollABallControls();
+
+            controls.Player.SetCallbacks(this);
+        }
+        controls.Player.Enable();
+    }
+
+    public void OnMove(InputAcion.CallbackContext context)
+    {
+       motion = context.ReadValue<Vector2>(); 
+    }
+
+   void FixedUpdate ()
+    {
+        Vector3 movement = new Vector3 (motion.x, 0.0f, motion.y);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive (false);
+            count = count + 1;
+            SetCountText ();
+        }
+    }
+    void SetCountText ()
+    {
+        countText.text = "Count:" + count.ToString ();
+        if (count >= 12)
+        {
+          winText.text = "You Win! Created by Shay Czachowski";
+        }
+    }
+}
